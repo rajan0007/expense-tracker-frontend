991a819b712fbca2840606f7f1bc304688ff4e4e
rajan0007 <bhalodiyarajan7@gmail.com>
2025-06-13 15:23:50 +0530

    code update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-backend-eta-rust.vercel.app/api",
+  baseURL: "https://expense-tracker-backend-pn6s.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
